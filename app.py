56eb6bb416e1b1294091215692a211ffafde0d7c
martonfarkas <14434@coderacademy.edu.au>
2023-06-06 12:19:41 +1000

    bcrypt

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -12,6 +13,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameg
 # db  is just a variable
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
+
+class User (db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('name', 'email', 'is_admin')
 
 # It has to inherit from db.Model
 class Card(db.Model):
@@ -36,6 +51,19 @@ def create_db():
 
 @app.cli.command('seed')
 def seed_db():
+    users = [
+        User(
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('spinynorman').decode('utf-8'),
+            is_admin=True
+        ),
+        User(
+            name='John Cleese',
+            email='cleese@spam.com',
+            password=bcrypt.generate_password_hash('tisbutascratch').decode('utf-8'),
+            is_admin=False
+        )
+    ]
     # Create an instance of the Card model in memory
     cards = [
         Card(
@@ -63,13 +91,28 @@ def seed_db():
 
     # Truncate the Card table
     db.session.query(Card).delete()
+    db.session.query(User).delete()
 
     # Add the card to the session (transaction)
     db.session.add_all(cards)
+    db.session.add_all(users)
 
-    # Committhe transaction to the database
+    # Commit the transaction to the database
     db.session.commit()
     print('Models seeded')
+
+
+@app.route('/register', methods = ['POST'])
+def register():
+    user_info = UserSchema().load(request.json)
+    user = User(
+        email=user_info.email,
+        password=bcrypt.generate_password_hash(user_info.password).decode('utf-8'),
+        name=user_info.name
+    )
+    print(user)
+    return {}
+
 
 @app.route('/cards')
 def all_cards():
