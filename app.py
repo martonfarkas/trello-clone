8b814e86613fce1fb4953bb35703727739202eee
martonfarkas <14434@coderacademy.edu.au>
2023-06-06 13:51:18 +1000

    error handling finished

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 
@@ -26,7 +27,7 @@ class User (db.Model):
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('name', 'email', 'is_admin')
+        fields = ('name', 'email', 'password', 'is_admin')
 
 # It has to inherit from db.Model
 class Card(db.Model):
@@ -104,14 +105,24 @@ def seed_db():
 
 @app.route('/register', methods = ['POST'])
 def register():
-    user_info = UserSchema().load(request.json)
-    user = User(
-        email=user_info.email,
-        password=bcrypt.generate_password_hash(user_info.password).decode('utf-8'),
-        name=user_info.name
-    )
-    print(user)
-    return {}
+    try:
+        # Parse, sanitize and validate the incoming JSON data
+        # via the schema
+        user_info = UserSchema().load(request.json)
+        # Create a new User model instance with the schema data
+        user = User(
+            email=user_info['email'],
+            password=bcrypt.generate_password_hash(user_info['password']).decode('utf-8'),
+            name=user_info['name']
+        )
+        # Add and commit the new user
+        db.session.add(user)
+        db.session.commit()
+
+        # Return the new user, excluding the passwords
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
 
 
 @app.route('/cards')
