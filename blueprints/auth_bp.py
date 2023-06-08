cc8ca2d7db8464a3522c1f0ba83360f7f8c45daa
martonfarkas <14434@coderacademy.edu.au>
2023-06-08 13:39:13 +1000

    refactoring done

--- blueprints/auth_bp.py
+++ blueprints/auth_bp.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
-from flask import request
+from flask import request, abort
 from sqlalchemy.exc import IntegrityError
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity
 from datetime import timedelta
 from models.user import User, UserSchema
 from init import db, bcrypt
@@ -42,3 +42,9 @@ def login():
     except KeyError:
         return {'error': 'Email and password are required'}, 400
 
+def admin_required():
+    user_email = get_jwt_identity()
+    stmt = db.select(User).filter_by(email=user_email)
+    user = db.session.scalar(stmt)
+    if not (user and user.is_admin):
+        abort(401)
